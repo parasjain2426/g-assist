f429b8c08c80c1ef278836b55b731b1bf238e1d8
Paras 1. Jain (Nokia) <paras.1.jain@nokia.com>
2022-12-13 11:57:01 +0530

    G-Assist initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+//request
+// {
+//   "handler": {
+//     "name": "handler_name"
+//   },
+//   "intent": {
+//     "name": "actions.intent.MAIN",
+//     "params": {},
+//     "query": ""
+//   },
+//   "scene": {
+//     "name": "SceneName",
+//     "slotFillingStatus": "UNSPECIFIED",
+//     "slots": {}
+//   },
+//   "session": {
+//     "id": "example_session_id",
+//     "params": {},
+//     "typeOverrides": []
+//   },
+//   "user": {
+//     "locale": "en-US",
+//     "params": {
+//       "verificationStatus": "VERIFIED"
+//     }
+//   },
+//   "home": {
+//     "params": {}
+//   },
+//   "device": {
+//     "capabilities": [
+//       "SPEECH",
+//       "RICH_RESPONSE",
+//       "LONG_FORM_AUDIO"
+//     ]
+//   }
+// }
+
+// response
+// {
+//   "session": {
+//     "id": "example_session_id",
+//     "params": {}
+//   },
+//   "prompt": {
+//     "override": false,
+//     "firstSimple": {
+//       "speech": "Hello World.",
+//       "text": ""
+//     }
+//   },
+//   "scene": {
+//     "name": "SceneName",
+//     "slots": {},
+//     "next": {
+//       "name": "actions.scene.END_CONVERSATION"
+//     }
+//   }
+// }
+
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const assistResponse = {
+  prompt: {
+    override: false,
+    firstSimple: {
+      speech: "Hello World.",
+      text: "Hello World",
+    },
+  },
+};
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
+app.post("/asked-query", (req, res) => {
+  //   console.log(req.body.message);
+  return res.json(assistResponse);
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server up and listening");
+});
